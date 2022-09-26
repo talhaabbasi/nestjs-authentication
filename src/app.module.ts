4f5f38f5ae01389af4fb723b94c5f7e326f59a5f
Talha Abbasi <talhaabbasi2009@hotmail.com>
2022-09-27 01:16:19 +0500

    [FEAT] Setup mongoose :muscle:

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/demo'),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
